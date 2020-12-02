3a03245ee59a8516af4f6040a401e4ab1634b7ff
DESKTOP-64APVNH\QUOC <quocitspkt@gmail.com>
2020-12-02 17:14:55 +0700

    Change Solution

--- MarketPlace/Controllers/HomeController.cs
+++ MarketPlace/Controllers/HomeController.cs
@@ -1,4 +1,4 @@
-﻿using eshopSoludtion.Webapp.Models;
+﻿using MarketPlace.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
@@ -7,7 +7,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace eshopSoludtion.Webapp.Controllers
+namespace MarketPlace.Controllers
 {
     public class HomeController : Controller
     {
